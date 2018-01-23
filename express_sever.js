61e7bd3495723e24a89d7e6d3809a6799fff613a
Waqas Bajwa <waqasbaj@gmail.com>
2018-01-23 19:48:33 +0000

    Added post to create an input form

--- express_sever.js
+++ express_sever.js
@@ -4,6 +4,12 @@ var PORT = process.env.PORT || 8080; // default port 8080
 
 app.set("view engine", "ejs")
 
+function generateRandomString() {
+
+}
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -20,12 +26,22 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", {templateVars : templateVars});
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars =
   { shortURL : req.params.id};
   res.render("urls_show");
 });
 
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 
 app.listen(PORT, () => {
