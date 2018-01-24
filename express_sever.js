281ce221b5b23b8e04a9dc639b900a46a04a0c71
Waqas Bajwa <waqasbaj@gmail.com>
2018-01-24 21:18:19 +0000

    Finished adding the Logout button

--- express_sever.js
+++ express_sever.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
-var PORT = process.env.PORT || 3000; // default port 8080
+var cookieParser = require('cookie-parser');
+
+var PORT = process.env.PORT || 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
@@ -17,31 +19,55 @@ function generateRandomString() {
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 var urlDatabase = { "b2xVn2": "http://www.lighthouselabs.ca",
                     "9sm5xK": "http://www.google.com"};
 
 
+var templateVars = { username: "Not Signed in"};
+
+
 
 app.get("/", (req, res) => {
+
   res.end("Hello!");
 });
 
 app.get("/urls", (req, res) => {
 
-  res.render("urls_index", {templateVars : urlDatabase});
+  if(req.cookies.username)
+  {
+     templateVars.username = req.cookies.username;
+  }
+
+  res.render("urls_index", {urlDatabase : urlDatabase, templateVars : templateVars});
+
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+
+
+  if(req.cookies.username)
+  {
+     templateVars.username = req.cookies.username;
+  }
+
+  res.render("urls_new", {templateVars : templateVars});
+
 });
 
 app.get("/urls/:id", (req, res) => {
   // var templateVars = { shortURL : req.params.id };
   console.log(req.params.id);
 
+  if(req.cookies.username)
+  {
+     templateVars.username = req.cookies.username;
+  }
+
   let id = req.params.id;
-  res.render("urls_show", {id : id});
+  res.render("urls_show", {id : id, templateVars : templateVars});
 });
 
 
@@ -76,6 +102,32 @@ app.post("/urls/:id", (req, res) => {
   urlDatabase[updateItem] = req.body.updateURL;
   res.redirect("/urls");
   console.log(urlDatabase);
+
+});
+
+app.post("/login", (req, res) => {
+
+
+  res.cookie("username", req.body.username);
+
+  console.log(req.cookies);
+
+
+  res.redirect("/urls");
+
+});
+
+app.post("/logout", (req, res) => {
+
+
+  res.clearCookie("username");
+
+  console.log(req.cookies);
+
+  templateVars.username = "Not Signed in";
+
+
+  res.redirect("/urls");
 
 });
 
